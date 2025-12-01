9968e922df44cce4a23f713d6aae6a08b75b479a
arc4e <arc4e@users.noreply.github.com>
2025-11-30 22:22:23 -0500

    Add local Brahmanandam reaction GIFs

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,25 +1,25 @@
 export const BRAHMI_REACTIONS = [
     {
         id: 'brahmi-confused',
-        url: 'https://media.tenor.com/images/f972557b649514736291684497645161/tenor.gif', // Confused
+        url: '/reactions/brahmi-confused.gif',
         label: 'Confused',
         type: 'reaction'
     },
     {
         id: 'brahmi-slap',
-        url: 'https://media.tenor.com/images/87766b37b4205934e622d9990146092a/tenor.gif', // Slap/Angry
+        url: '/reactions/brahmi-angry.gif',
         label: 'Angry',
         type: 'reaction'
     },
     {
         id: 'brahmi-laugh',
-        url: 'https://media.tenor.com/images/4d2639202810575d84c662886c757c28/tenor.gif', // Laugh
+        url: '/reactions/brahmi-laugh.gif',
         label: 'LOL',
         type: 'reaction'
     },
     {
         id: 'brahmi-dance',
-        url: 'https://media.tenor.com/images/b8c4f944905aa45279cb65045e05c83d/tenor.gif', // Dance
+        url: '/reactions/brahmi-party.gif',
         label: 'Party',
         type: 'decoration'
     }
